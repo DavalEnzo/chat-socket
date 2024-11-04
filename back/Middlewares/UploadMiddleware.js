51ec2a3f3968cb0f4b9a7617f4c79f890b5554c9
DavalEnzo <davalenzo@gmail.com>
2024-11-05 00:57:11 +0100

    création des dossiers si existe pas

--- back/Middlewares/UploadMiddleware.js
+++ back/Middlewares/UploadMiddleware.js
@@ -1,16 +1,18 @@
 const multer = require('multer');
-const path = require("node:path");
+const path = require('node:path');
+const fs = require('fs');
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, path.join(__dirname, "../uploads/profilePictures/"));
+    const uploadPath = path.join(__dirname, '../uploads/profilePictures/');
+    fs.mkdirSync(uploadPath, { recursive: true });
+    callback(null, uploadPath);
   },
   filename: (req, file, callback) => {
     callback(null, file.originalname);
   },
 });
 
-//   const upload = multer({ storage :storage }); OR
 const upload = multer({ storage });
 
 module.exports = upload;
