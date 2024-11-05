dd62fa0714d5f3e170e2b5dc8e42bff4f75f4658
DavalEnzo <davalenzo@gmail.com>
2024-11-05 14:03:37 +0100

    samesite none

--- back/Controllers/AuthController.js
+++ back/Controllers/AuthController.js
@@ -42,7 +42,7 @@ module.exports.Login = async (req, res, next) => {
     res.cookie("token", token, {
       withCredentials: true,
       httpOnly: false,
-      sameSite: "strict",
+      sameSite: "none",
     });
     res.status(201).json({ user: user, message: "Vous êtes connecté !", success: true });
     next()
