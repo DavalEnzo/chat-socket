cacfba875d403e866393fe678d9bbda74bc0dd96
DavalEnzo <davalenzo@gmail.com>
2024-11-05 01:51:06 +0100

    suppression cookie fonctionne pas avec render

--- front/src/components/NavbarComponent.js
+++ front/src/components/NavbarComponent.js
@@ -79,7 +79,7 @@ export default function NavbarComponent({ profilePic, socket }) {
               </div>
             </div>
           </div>
-          { (cookies["token"] !== 'undefined' && user) &&
+          { user &&
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             <button
               type="button"
